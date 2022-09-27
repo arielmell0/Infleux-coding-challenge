3a8fcced8237c5b6ed9725146ce815b12255ded9
arielmell0 <arielmello@rede.ulbra.br>
2022-09-27 20:39:20 -0300

    feat: adicionando o nosso front-end

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './server'
-
-const port = process.env.PORT || 3000
-
-app.on('databaseConnected', () => {
-    app.listen(port, () => {
-        console.log(`Ads Server running at port ${port}`)
-    })
-})
