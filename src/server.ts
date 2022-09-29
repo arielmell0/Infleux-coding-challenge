e32541c2458bec133aa58accb3d8ee02c9dcf9cc
arielmell0 <arielmello@rede.ulbra.br>
2022-09-29 20:01:27 -0300

    feat: refatorando o cors

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,7 @@ import mongoose from 'mongoose'
 
 const app = express();
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+app.use(cors())
 app.use(express.json(), urlencoded({ extended: true }))
 
 app.use(route)
