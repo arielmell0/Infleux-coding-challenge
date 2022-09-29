0ddf6337c77e80a1cea81c8c92c2b8461bccb8b6
arielmell0 <arielmello@rede.ulbra.br>
2022-09-29 19:03:28 -0300

    feat: adicionando cors ao projeto

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,30 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-import express, { urlencoded } from 'express';
+import express, { urlencoded } from 'express'
+import cors from 'cors'
 import route from './routes'
 import mongoose from 'mongoose'
 
 const app = express();
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(express.json(), urlencoded({ extended: true }))
+
 app.use(route)
+
 
 mongoose.connect(process.env.CONNECTIONSTRING as string)
     .then(() => {
         app.emit('databaseConnected')
         console.log('Connected to the database')
     })
-    .catch((e) => console.log('Ops, falha ao conectar ao banco de dados: ', e))
+    .catch((e: any) => console.log('Ops, falha ao conectar ao banco de dados: ', e))
 
 export default app;
